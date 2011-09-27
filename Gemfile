ced8a0912ebc9463497cc0bb6be9122699fde511
Kevin Kelly <kkelly18@gmail.com>
2011-09-26 21:01:43 -0400

    Rails app structure and gemfile

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'gravatar_image_tag', '~> 1.0.0'
+gem "will_paginate", "~> 3.0.1"
+gem "pg", "~> 0.11.0"
+
+gem "faker", "~> 1.0.0"
+
+# To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
+# gem 'ruby-debug'
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
+group :development, :test do
+  gem "rspec-rails", "~> 2.6.1"
+  gem "capybara", "~> 1.0.1"
+  gem "factory_girl_rails", "~> 1.2.0"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sirocco::Application.config.secret_token = 'bc77c53e6b78b14d728040f536c033119916a01fd61c1c10f249b860eff874373637730002d967a1dd8f966c78fb835a297411ff9b65304188509c5b4a5876c7'
