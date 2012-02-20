6107899f8b13c8ef2e1acca6871612ca15d8f76e
Kevin Kelly <kkelly18@gmail.com>
2012-02-19 20:58:45 -0500

    more SASS refactoring

--- spec/requests/user_account_spec.rb
+++ spec/requests/user_account_spec.rb
@@ -25,7 +25,7 @@ describe "Accounts#Show" do
 
   describe "ACCOUNTS" do
     it "should show two accounts" do
-      page.should have_selector("td.index_item", :count=>2)
+      page.should have_selector("td.item", :count=>2)
     end
     it "should navigate signedin user to their personal account" do
       click_link   "Personal Account"
@@ -48,20 +48,20 @@ describe "Accounts#Show" do
     end
     it "should show a list of projects sponsored by this account" do
       click_link   "#{@account.name}"
-      page.should have_selector("td.index_item", :count=>3)
+      page.should have_selector("td.item", :count=>3)
     end
     it "should only show projects sponsored by this account" do
       new_account     = create_account(@user)
       different_sponsorship_same_user = create_sponsorship(new_account, @user, @user)
       new_project_same_user_sponsored_by_new_account = create_sponsored_project(new_account, @user)
       click_link   "#{@account.name}"
-      page.should have_selector("td.index_item", :count=>3)
+      page.should have_selector("td.item", :count=>3)
     end
     it "should enable withdraw commmand" do
       click_link   "#{@account.name}"
-      item = page.find('td.index_item', :text => "#{@projects[0].name}")
+      item = page.find('td.item', :text => "#{@projects[0].name}")
       item.click_link('Withdraw')
-      item = page.find('td.index_item', :text => "#{@projects[0].name}")
+      item = page.find('td.item', :text => "#{@projects[0].name}")
       item.find('a', :text => 'Rejoin')
     end
 
@@ -77,7 +77,7 @@ describe "Accounts#Show" do
         page.should_not have_field('project_name')
       end
       it "should not show suspend command on items" do
-        item = page.find('td.index_item', :text => "#{@projects[0].name}")
+        item = page.find('td.item', :text => "#{@projects[0].name}")
         item.should_not have_link('Suspend')
       end
     end
@@ -95,21 +95,21 @@ describe "Accounts#Show" do
         fill_in "project_name", :with => 'test_project'
         click_button "Create"
         page.should have_selector("div.flash.success", :text => "test_project")        
-        page.should have_selector("td.index_item", :count=>4)
+        page.should have_selector("td.item", :count=>4)
       end
       it "should enable suspend command" do
-        item = page.find('td.index_item', :text => "#{@projects[0].name}")
+        item = page.find('td.item', :text => "#{@projects[0].name}")
         item.click_link('Suspend')
-        item = page.find('td.index_item', :text => "#{@projects[0].name}")
+        item = page.find('td.item', :text => "#{@projects[0].name}")
         item.should have_link('Reinstate')
       end
       it "should reinstate a suspended project" do
-        item = page.find('td.index_item', :text => "#{@projects[0].name}")
+        item = page.find('td.item', :text => "#{@projects[0].name}")
         item.click_link('Suspend')
-        item = page.find('td.index_item', :text => "#{@projects[0].name}")
+        item = page.find('td.item', :text => "#{@projects[0].name}")
         item.should have_link('Reinstate')
         item.click_link('Reinstate')
-        item = page.find('td.index_item', :text => "#{@projects[0].name}")
+        item = page.find('td.item', :text => "#{@projects[0].name}")
         item.should have_link('Suspend')
       end
     end
@@ -132,14 +132,14 @@ describe "Accounts#Show" do
     it "should show a list of users" do
       click_link("#{@account.name}")
       click_link("SHOW TEAM")
-      page.should have_selector("td.index_item", :count=>4)
+      page.should have_selector("td.item", :count=>4)
     end
     it "should not show a user that isn't in this account" do
       @users << create_user
       click_link("#{@account.name}")
       click_link("SHOW TEAM")
-      page.should have_selector("td.index_item", :count=>4)
-      page.should_not have_selector("td.index_item", :text => "#{@users.last.name}")
+      page.should have_selector("td.item", :count=>4)
+      page.should_not have_selector("td.item", :text => "#{@users.last.name}")
     end
 
     describe "where current user is not account admin" do
@@ -154,7 +154,7 @@ describe "Accounts#Show" do
         page.should_not have_field('sponsorship_user_email')
       end     
       it "should not show suspend command on items" do
-        item = page.find('td.index_item', :text => "#{@users.last.name}")
+        item = page.find('td.item', :text => "#{@users.last.name}")
         item.should_not have_link('Suspend')
       end 
     end
@@ -167,10 +167,10 @@ describe "Accounts#Show" do
         page.should have_field('sponsorship_user_email')  
       end
       it "should uninvite a user from the account" do
-        item = page.find('td.index_item', :text => "#{@users.last.name}")
+        item = page.find('td.item', :text => "#{@users.last.name}")
         item.should have_link('Uninvite')
         item.click_link('Uninvite')
-        item = page.find('td.index_item', :text => "#{@users.last.name}")
+        item = page.find('td.item', :text => "#{@users.last.name}")
         item.should have_link('Invite')
       end
       it "should add another user" do
@@ -179,8 +179,8 @@ describe "Accounts#Show" do
         fill_in "sponsorship_user_email", :with => "#{@users.last.email}"
         click_button "Invite"
         page.should have_selector("div.flash.success", :text => "#{@users.last.name}")        
-        page.should have_selector("td.index_item", :count=>5)
-        item = page.find('td.index_item', :text => "#{@users.last.name}")
+        page.should have_selector("td.item", :count=>5)
+        item = page.find('td.item', :text => "#{@users.last.name}")
         item.should have_link('Uninvite')
       end
     end
